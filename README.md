b6a9d2c1a7c058b3fd177da4211b601d6696be20
Ayushika <ayushikabansal7890@gmail.com>
2023-06-24 16:15:22 +0530

    updated functionality and readme

--- README.md
+++ README.md
@@ -2,3 +2,4 @@
 
 1. Random Quote Generator
 2. Custom Modal
+3. Accordian (FAQ)
--- script/project2.js
+++ script/project2.js
@@ -4,12 +4,17 @@ const modalContainer = document.getElementById('modal-container');
 
 openBtn.addEventListener('click', () => {
     modalContainer.style.display = 'block';
+    openBtn.style.display = 'none';
 });
 
 closeBtn.addEventListener('click', () => {
     modalContainer.style.display = 'none';
+    openBtn.style.display = 'block';
 });
 
 window.addEventListener('click', (e) => {
-    if(e.target === modalContainer) modalContainer.style.display = 'none';
+    if(e.target === modalContainer) {
+        modalContainer.style.display = 'none';
+        openBtn.style.display = 'block';
+    }
 });
